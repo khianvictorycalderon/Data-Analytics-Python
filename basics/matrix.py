1fecb57e00e1bc72265ea0db7ca29987beb74164
khian calderon <ckhianvictory@gmail.com>
2025-11-26 09:35:44 +0800

    Added sample data

---
+++ basics/matrix.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sample data
+people = {
+    "Mark": 0,
+    "Sean": 1,
+    "Dishiela": 2,
+    "Azil": 3,
+    "Jayvee": 4
+}
+
+# Allowace in one week
+allowance = np.array([
+    [200, 400, 150, 300, 600],
+    [400, 200, 300, 350, 150],
+    [250, 300, 100, 200, 300],
+    [150, 200, 350, 400, 550],
+    [300, 250, 150, 200, 350]
+])
